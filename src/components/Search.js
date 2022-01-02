95c353aeb04a97b4063a89105224b96205f60bf5
Mustafa Emir <info@mustafaemir.com>
2022-01-02 23:51:00 +0300

    Make the autocomplete search component work with fake data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,73 @@
+import {useEffect, useState, useRef} from 'react'
 import './Search.css';
 
+const data = [
+  {
+    id: 1,
+    title: 'Test 1'
+  },
+  {
+    id: 2,
+    title: 'Test 2'
+  },
+  {
+    id: 3,
+    title: 'Test 3'
+  },
+  {
+    id: 4,
+    title: 'Test 4'
+  }
+];
+
 function Search() {
+  const [search, setSearch] = useState('');
+  const [result, setResult] = useState([]);
+  const searchRef = useRef();
+
+  const isTyping = search.replace(/\s+/, '').length > 0;
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutSide);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutSide);
+    }
+  }, []);
+
+  const handleClickOutSide = (e) => {
+    if(searchRef.current && !searchRef.current.contains(e.target)){
+      setSearch('');
+    }
+  }
+
+  useEffect(() => {
+    if(isTyping){
+      const filteredResult = data.filter(item => item.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+      setResult(filteredResult.length > 0 ? filteredResult : false);
+    } else {
+      setResult([]);
+    }
+  }, [search])
+
   return (
     <>
-      <div className="search">
-        <input type="text" placeholder="Bir şeyler ara..." />
+      <div className="search" ref={searchRef}>
+        <input type="text" value={search} className={isTyping ? 'typing' : null} placeholder="Bir şeyler ara..." onChange={(e) => setSearch(e.target.value)} />
+        {isTyping && (
           <div className="search-result">
-              <div className="search-result-item">
+            {result && result.map(item=> (
+              <div key={item.id} className="search-result-item">
+                {item.title}
+              </div>
+            ))}
 
-              </div>
+            {!result && (
               <div className="result-not-found">
-                '....' ile ilgili bir şey bulamadık...
+                '{search}' ile ilgili bir şey bulamadık...
               </div>
+            )}
           </div>
+        )}
       </div>
     </>
   )
